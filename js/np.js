f4917863b52bf47deff2dd585e78543e5d1f29b4
QuangS2 <2051052108Quang@ou.edu.vn>
2021-12-04 14:25:37 +0700

    cart.html bo khung

--- js/np.js
+++ js/np.js
@@ -1,9 +1,22 @@
-
 $(function () {
+  //-menubar
+  $(".-menubar li,.-brand-logo a").hover(
+    function () {
+      $(this).addClass("hover");
+    },
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+  $(".-menubar li,.-brand-logo a").click(function (e) { 
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
+  });
+  //slide
   $(".-leftBtn").click(function () {
     var limit = Number($(".-img-sp").attr("max-slot"));
     var now = Number($(".-img-sp")[0].slot);
-    var pre = now-1;
+    var pre = now - 1;
     if (pre < 1) pre = limit;
     $(".-img-sp-content.rightSide").removeClass("rightSide top effect");
     $(".-img-sp-content[slot=" + pre + "]").addClass("leftSide");
@@ -12,13 +25,12 @@ $(function () {
       $(".-img-sp-content[slot=" + pre + "]").addClass("top effect");
       $(".-img-sp-content[slot=" + pre + "]").removeClass("leftSide");
     });
-    $(".-img-sp").attr('slot', pre);
+    $(".-img-sp").attr("slot", pre);
     $(".-sp-pro>.-bar-stick >span.active").removeClass("active");
-    $(".-sp-pro>.-bar-stick >span[slot="+pre+"]").addClass("active");
+    $(".-sp-pro>.-bar-stick >span[slot=" + pre + "]").addClass("active");
   });
   $(".-rightBtn").click(function () {
     changSlide(Number($(".-img-sp").attr("slot")) + 1, "-img-sp");
-    
   });
   $(".-bar-stick span").click(function () {
     var slot = Number($(this)[0].slot);
@@ -32,6 +44,17 @@ $(function () {
     changSlide(Number($(".-img-sp").attr("slot")) + 1, "-img-sp");
     changSlide(Number($(".-img-ad").attr("slot")) + 1, "-img-ad");
   }, 5000);
+  //-btn
+  $(".-btn").hover(function () {
+      $(this).addClass("hover");
+    }, function () {
+      $(this).removeClass("hover");
+    }
+  );
+  $(".-btn").click(function (e) { 
+    $(".-btn.active").removeClass("active");
+    $(this).addClass("active");
+  });
 });
 function changSlide(n, clName) {
   var limit = Number($("." + clName).attr("max-slot"));
@@ -46,9 +69,11 @@ function changSlide(n, clName) {
       $("." + clName + ">div[slot=" + n + "]").removeClass("rightSide");
     });
     $("." + clName).attr("slot", n);
-    var clParent =$("."+clName).parent().attr("class").replace(/\s/g, ".");
-    $("."+clParent+">.-bar-stick >span.active").removeClass("active");
-    $("."+clParent+">.-bar-stick >span[slot="+n+"]").addClass("active");
-    console.log(clParent);
+    var clParent = $("." + clName)
+      .parent()
+      .attr("class")
+      .replace(/\s/g, ".");
+    $("." + clParent + ">.-bar-stick >span.active").removeClass("active");
+    $("." + clParent + ">.-bar-stick >span[slot=" + n + "]").addClass("active");
   }
 }
